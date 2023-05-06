fe47a03a09b4dee9df8bf818a987f6bb5b2d3055
Konstantin <132730553+HowVreYou@users.noreply.github.com>
2023-05-06 09:55:09 +0500

    Update kod.py
    Шестой шаг кода

--- kod.py
+++ kod.py
@@ -59,7 +59,8 @@ while game:
         ball.rect.y += dy
         if ball.rect.y<0 or ball.rect.y>=wind_h - 50:
             dy *= -1
-
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            dx *= -1
 
         wind.fill((255, 255, 255))
         racket1.reset()
@@ -69,5 +70,7 @@ while game:
 
     display.update()
     timer.tick(FPS)
+
+
 
 
